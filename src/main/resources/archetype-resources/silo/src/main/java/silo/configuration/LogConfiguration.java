d5e471bfef200304446be7845a826e9ae797136f
Björn Häuser <b.haeuser@rebuy.de>
2013-10-25 10:11:12 +0200

    remove bridge

--- src/main/resources/archetype-resources/silo/src/main/java/silo/configuration/LogConfiguration.java
+++ src/main/resources/archetype-resources/silo/src/main/java/silo/configuration/LogConfiguration.java
@@ -8,7 +8,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
