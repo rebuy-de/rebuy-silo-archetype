fd0db4c508fcbeac3dc2cab3b36ec3860fc88899
Hendrik Pannhorst <h.pannhorst@reBuy.de>
2016-02-02 18:18:31 +0100

    add consul to the archetype

---
+++ src/main/resources/archetype-resources/silo/src/main/java/silo/configuration/settings/ConsulSettings.java
@@ -0,0 +1,27 @@
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+
+package ${package}.${artifactId}.configuration.settings;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("consul")
+public class ConsulSettings
+{
+    public String name;
+
+    public String agent;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAgent(String agent)
+    {
+        this.agent = agent;
+    }
+}
---
+++ src/main/resources/archetype-resources/silo/src/test/java/silo/annotation/UnitTest.java
@@ -0,0 +1,8 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${artifactId}.annotation;
+
+public interface UnitTest
+{
+}
