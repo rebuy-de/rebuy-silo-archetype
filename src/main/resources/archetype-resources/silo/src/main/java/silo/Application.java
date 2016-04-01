d2d070ff4bcaab3aaf271fbe77860550b507fa17
Sven Walter <s.walter@rebuy.de>
2016-04-01 14:32:08 +0200

    enforce UTF8 as JVM charset

--- src/main/resources/archetype-resources/silo/src/main/java/silo/Application.java
+++ src/main/resources/archetype-resources/silo/src/main/java/silo/Application.java
@@ -8,12 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.nio.charset.Charset;
+
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application
 {
     public static void main(String[] args)
     {
+        if (!Charset.forName("UTF-8").equals(Charset.defaultCharset())) {
+            throw new RuntimeException("Default charset must be UTF-8");
+        }
+
         new SpringApplicationBuilder()
             .bannerMode(Banner.Mode.OFF)
             .sources(Application.class)
