3a43d3f0373a74493936722104f85a9b25973c1e
Björn Häuser <b.haeuser@rebuy.com>
2020-07-31 18:15:52 +0200

    Replace sandbox.rebuy.loc with drybox.cloud

--- src/main/resources/archetype-resources/deployment/multistage/sandbox.rb
+++ src/main/resources/archetype-resources/deployment/multistage/sandbox.rb
@@ -1,4 +1,4 @@
 prompt_if_unset :host
-server "#{host}.sandbox.rebuy.loc", :web, :app
+server "#{host}.drybox.cloud", :web, :app
 set :deploy_to, '/opt/services/${artifactId}-silo'
-set :fqdn, "#{host}.sandbox.rebuy.loc"
+set :fqdn, "#{host}.drybox.cloud"
